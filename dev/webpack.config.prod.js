71e76902f4c99645c6851585765aefde929ad4bf
佐藤正康 <satohmsys@gmail.com>
2018-04-20 14:54:10 +0900

    webpack config

---
+++ dev/webpack.config.prod.js
@@ -0,0 +1,44 @@
+const merge = require( 'webpack-merge' );
+const path	= require( 'path');
+const baseConfig = require( './webpack.config.common.js' );
+const MODE = 'production';
+
+let vars = require('./webpack.variables.js');
+
+vars.PATHS.output.path = '../dist';
+vars.enabledSourceMap = false;
+
+let config = merge( baseConfig, {
+	mode: MODE,
+
+	module: {
+		rules:[
+			{
+				test: /\.(eot|woff|ttf)$/,
+				exclude: /node_modules\/^(slick-carousel)/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						publicPath: '/',
+						context: 'src',
+						name: vars.PATHS.dir.output + '/fonts/[name].[ext]'
+					}
+				}
+			},
+			{
+				test: /\.( gif|jpg|png|svg|ico)$/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						publicPath: '/',
+						context: 'src',
+						name: vars.PATHS.dir.output + '/[path][name].[ext]'
+					}
+				}
+			}
+		]
+	}	
+});
+
+
+module.exports = config;
